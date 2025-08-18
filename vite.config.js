47d5ea3e11a0f7ee2c4f744ced33d132328bf40a
Sonnur Gülen <96986979+Sonnurg@users.noreply.github.com>
2025-08-18 10:22:49 +0000

    4

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mutabakat-figma/', // GitHub repository adınızı yazın
+  base: "./",   // ✅ Netlify için doğru
   build: {
-    outDir: 'dist'
+    outDir: "dist"
   }
 })
